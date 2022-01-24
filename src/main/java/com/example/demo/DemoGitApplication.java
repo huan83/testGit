bb9cffece395a3102eb036af32f8efa1192e5c20
huan <huanchen.tx@gmail.com>
2022-01-24 12:59:22 -0600

    delete message

--- src/main/java/com/example/demo/DemoGitApplication.java
+++ src/main/java/com/example/demo/DemoGitApplication.java
@@ -9,11 +9,7 @@ public class DemoGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGitApplication.class, args);
 		System.out.println("test dev fix bug");
-		System.out.println("test2111 dev fix bug");
-		System.out.println("test333 dev fix bug");
-		
-		System.out.println("add something to bug fix branch");
-		System.out.println("add something11 to bug fix branch");
+
 	}
 
 }
